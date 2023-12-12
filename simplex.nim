a918fc274f641385fa19e686f59d2e3e75e8e69f
кiрыла <90193031+9Azzing6@users.noreply.github.com>
2023-12-12 13:00:23 +0300

    upd

--- simplex.nim
+++ simplex.nim
@@ -1,22 +1,18 @@
 import matrixLib
 
-# var dataF = @[4.0, -1.0, -1.0, 1.0, 1.0, 0.0, 7, -1.0, 2.0, 1.0, 0.0, 1.0, 7.0, 2.0, -1.0, 0.0, 1.0, 1.0, 5.0, -3.0, 1.0, 0.0, 0.0, 0.0]
+var dataF = @[4.0, -1.0, -1.0, 1.0, 1.0, 0.0, 7, -1.0, 2.0, 1.0, 0.0, 1.0, 7.0, 2.0, -1.0, 0.0, 1.0, 1.0, 5.0, -3.0, 1.0, 0.0, 0.0, 0.0]
 
-var data = @[2.0, 1.0, -2.0, 2.0, -2.0, 1.0, 5.0, 1.0, 1.0, 5.0, -3.0, 1.0]
-var mat: matrix = raspMatrix(4, 3, data)
+# var data = @[2.0, 1.0, -2.0, 2.0, -2.0, 1.0, 5.0, 1.0, 1.0, 5.0, -3.0, 1.0]
+var mat: matrix = raspMatrix(4, 6, dataF)
 # var mat2: matrix = mat
 
 proc fRowCheck(m: matrix): tuple[check: bool, j_ind: int] = #проверка опорного плана на оптимальность и нахождения индекса отрицательного элемента в f-строке, если таковой имеется
     result.check = false
-    result.j_ind = -1
+    result.j_ind = 1
 
     var max = 0.0
 
     for j in 0 .. m.getColumns() - 1:
-        # if m[m.getRows() - 1, j] < 0:
-        #     echo "hello from check"
-        #     echo m[m.getRows() - 1, j]
-            
         if m[m.getRows() - 1, j]  < max:
             max = m[m.getRows() - 1, j]
             result.check = true
@@ -25,7 +21,7 @@ proc fRowCheck(m: matrix): tuple[check: bool, j_ind: int] = #проверка о
 proc inf_funcCheck(m: matrix, j_ind: int): bool = #проверка функции на неограниченность
     result = true
     for i in 0 .. m.getRows() - 1:
-        if m[i, j_ind] > 0:
+        if (m[i, j_ind] > 0 and m[i, 0] > 0) or (m[i, j_ind] < 0 and m[i, 0] < 0):
             result = false
             break
 
@@ -35,7 +31,7 @@ proc indexSO(m: matrix, j_ind: int): int = #рассчет СО и выбор р
         so: float = 0.0
         min: float = Inf
     for i in 0 .. m.getRows() - 1:
-        if (m[i, 0] > 0 and m[i, j_ind] > 0) or (m[i, 0] < 0 and m[i, j_ind] < 0):
+        if ((m[i, 0] > 0 and m[i, j_ind] > 0) or (m[i, 0] < 0 and m[i, j_ind] < 0)) and m[i, j_ind] != 0:
             so = m[i, 0] / m[i, j_ind]
             if so < min:
                 min = so
@@ -76,8 +72,8 @@ proc desiredLength(str: string, des_len: int): int =
 
 proc simplexTableToString(m: matrix, i_ind: int, j_ind: int, hor_x, vert_x: seq[string]): string =
     var 
-        shapka: seq[string] = @[ "1"]
-        desired_lenght: int = 20
+        shapka: seq[string] = @["1"]
+        desired_lenght: int = 8
     # делаем шапочку
 
     result = "БП"
@@ -117,16 +113,60 @@ proc simplexTableToString(m: matrix, i_ind: int, j_ind: int, hor_x, vert_x: seq[
         if i != m.getRows() - 1:
             result.add("\n")    
 
-proc checkOP(vert_x: seq[string]): bool =
-    result = false
-    if "0" in vert_x:
-        result = true
+proc checkOP(vert_x: seq[string]): tuple[check: bool, i_indeces_zero: seq[int]] =
+    result.check = false
+    for zero in 0 .. len(vert_x) - 1:
+        if vert_x[zero] == "0":
+            result.check = true
+            result.i_indeces_zero.add(zero)
+    if result.check:
+        echo "опорный план отсуствует"
+    else: echo "опорный план присуттсвует"
 
-proc findOP(m: matrix, hor_x, vert_x: seq[string]): matrix =
+proc terminateNullColumn(m: matrix, hor_x: var seq[string]): matrix =
+    result = raspMatrix(m.getRows(), m.getColumns() - 1)
+    var j_ind: int
+    for x in 0 .. len(hor_x) - 1:
+        if hor_x[x] == "0":
+            j_ind = x + 1
+            break
+    hor_x.delete(j_ind - 1)
+
+
+    for i in 0 .. result.getRows() - 1:
+        for j in 0 .. result.getColumns() - 1:
+            if j == j_ind: continue
+            else: continue
+                
+    echo "hello from terminator 2: "
+    echo result.matrixToString()
+    echo "hello from terminator 3: "
+    echo m.matrixToString()
+
+proc findOP(m: var matrix, hor_x, vert_x: var seq[string]): matrix =
+    var iteration: int = 0
+    var j_ind: int
     echo "поиск опорного плана"
-    result = raspMatrix(2, 2)
+    while checkOP(vert_x).check:
+        echo "поиск"
+        echo "iteration: ", iteration
+        for j in 1 .. m.getColumns() - 1:
+            if (indexSO(m, j) > 0) and (indexSO(m, j) in checkOP(vert_x).i_indeces_zero):
+                j_ind = j
+                break
+        echo simplexTableToString(m, indexSO(m, j_ind), j_ind, hor_x, vert_x)
+        swapX(hor_x, vert_x, indexSO(m, j_ind), j_ind)
+        m = jordan(m, indexSO(m, j_ind), j_ind)
+        m = terminateNullColumn(m, hor_x)
+        iteration += 1
+    echo "был получен опорный план"
+    echo "табличка, содержащая опорный план:"
+    echo simplexTableToString(m, -1, -1, hor_x, vert_x)
+    
 
-proc firstTry(m: var matrix): void =
+    result = m
+
+proc firstTry(m: var matrix, hor_x, vert_x: var seq[string]): void =
     var
         hor_x: seq[string] = @["x1", "x2"]
         vert_x: seq[string] = @["x4", "x3", "x5"]
@@ -145,10 +185,14 @@ proc firstTry(m: var matrix): void =
         iteration += 1
     echo "был получен оптимальный план"
     echo "финальная табличка: "
-    echo simplexTableToString(m, -1, -1,hor_x, vert_x)
+    echo simplexTableToString(m, -1, -1, hor_x, vert_x)
 
 
-# echo mat.matrixToString()
-echo "*******************************"
 
-firstTry(mat)
+# echo "*******************************"
+
+# firstTry(mat, hor_x, vert_x)
+var
+        hor_x: seq[string] = @["x1", "x2", "x3", "x4", "x5"]
+        vert_x: seq[string] = @["0", "0", "0"]
+echo findOP(mat, hor_x, vert_x).matrixToString
